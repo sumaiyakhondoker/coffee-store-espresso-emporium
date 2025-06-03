083e619672462285721d9091e3398febd9b41390
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2025-06-03 20:10:23 +0600

    popular products section added successfully

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import PopularProducts from './PopularProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            this is home
+            <PopularProducts></PopularProducts>
         </div>
     );
 };
